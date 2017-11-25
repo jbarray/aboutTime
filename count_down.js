b34b4627c3bb791f915375800e2c4db105387c6c
jby <1614963384@qq.com>
2017-11-25 17:37:12 +0800

    倒计时,距离某个日期还有多少天

--- count_down.js
+++ count_down.js
@@ -1,26 +1,40 @@
 //获得数据
-var thisTime=document.getElementById('now-time');
-var leaveTime=document.getElementById('leave-days');
-var leaveSeconds=document.getElementById('leave-seconds');
+// var leaveSeconds=document.getElementById('leave-seconds');
 
 //输出现在的时间
 //思路:先创建一个new date() ;获取他的方法:年 月 日等. 注意:星期需要转换!
 function time(){
     var getTime=new Date();
-    // thisTime.innerHTML=getTime;
     var year=getTime.getFullYear();
     var month=getTime.getMonth();
     var date=getTime.getDate();
     var hour=getTime.getHours();
+    hour=check(hour);
     var minute=getTime.getMinutes();
+    minute=check(minute);
     var second=getTime.getSeconds();
+    second=check(second);
     document.getElementById('now-time').innerHTML=year+'年'+(month+1)+'月'+date+'日'+hour+'时'+minute+'分'+second+'秒';
 }
 //用setTimeOut进行循环,让出现动态的效果.
 function repeat(){
     setInterval(time,1000);
 }
+//为<10的数字前加0
+function check(i){
+    if(i<10){
+        i='0'+i;
+    }
+    return i;
+}
 repeat();
 
 //倒计时
 //思路:用截止时间减去现在的时间
+function countDown() {
+    var deadline=new Date('2019,1,1');//截止时间
+    var nowTime=new Date();//当前时间
+    document.getElementById('leave-days').innerHTML=Math.ceil((deadline.getTime() - nowTime.getTime()) / 86400000);//
+    //截止时间-当前时间 再转化成毫秒:getTime() ;将毫秒转换成天:86400000 ;进行向上取整数:Math.ceil;
+}
+countDown();
