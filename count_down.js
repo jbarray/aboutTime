bd2cc7b8742a0bd4e439f36a591dd763afd0766b
jby <1614963384@qq.com>
2017-11-27 11:20:15 +0800

    1.实现功能:倒计时,距离某个日期还有具体多少时间 2.调整页面样式

--- count_down.js
+++ count_down.js
@@ -34,7 +34,27 @@ repeat();
 function countDown() {
     var deadline=new Date('2019,1,1');//截止时间
     var nowTime=new Date();//当前时间
-    document.getElementById('leave-days').innerHTML=Math.ceil((deadline.getTime() - nowTime.getTime()) / 86400000);//
+    document.getElementById('leave-days').innerHTML=Math.ceil((deadline.getTime() - nowTime.getTime()) / 86400000);
     //截止时间-当前时间 再转化成毫秒:getTime() ;将毫秒转换成天:86400000 ;进行向上取整数:Math.ceil;
 }
 countDown();
+
+//限时抢购
+//思路:和倒计时思路相同.
+function count(){
+    var deadline=new Date('2018/2/22 00:00:00');//截止的具体时间
+    var nowTime=new Date();//当前时间
+    var time=Math.floor((deadline.getTime()-nowTime.getTime())/1000);
+    var day=Math.floor(time/(24*60*60));
+    var hour=Math.floor((time-day*24*60*60)/(60*60));
+    hour=check(hour);
+    var minute=Math.floor((time-day*24*60*60-hour*60*60)/60);
+    minute=check(minute);
+    var second=Math.floor(time-day*24*60*60-hour*60*60-minute*60);
+    second=check(second);
+    document.getElementById('leave-seconds').innerHTML=day+'天'+hour+'时'+minute+'分'+second+'秒';
+}
+function set(){
+    setInterval(count,1000);
+}
+set();
